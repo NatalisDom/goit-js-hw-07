f0478afcbea4d2a077e857e06e58335561808474
Наталія Яловега <109685236+NatalisDom@users.noreply.github.com>
2023-01-13 01:46:21 +0200

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+let imgPainting = "";
+let part;
+
+
+gallery.insertAdjacentHTML(
+    "beforeend",
+    galleryItems
+      .map(
+        ({ preview, original, description }) =>
+          `
+          <div class="gallery__item">
+            <a class="gallery__link"  href="${original}">
+              <img
+                class="gallery__image"
+                src="${preview}"
+                data-painting="${original}"
+                alt="${description}"
+              />
+            </a>
+          </div>
+          `
+      )
+      .join("")
+  );
+
+  gallery.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (!e.target.classList.contains("gallery__image")) return;
+  
+    imgPainting = e.target.dataset.painting;
+    const clickEscape = ({ code }) => {
+      if (code === "Escape" && part.visible()) part.close();
+    };
+  
+    part = basicLightbox.create(
+      `<img src="${imgPainting}" width="800" height="600">`,
+      {
+        onShow: () => {
+          // console.log("Show modal.");
+          document.addEventListener("keydown", clickEscape);
+        },
+        onClose: () => {
+          // console.log("Close modal.");
+          document.removeEventListener("keydown", clickEscape);
+        },
+      }
+    );
+  
+    // console.log(instance);
+    part.show();
+  });  
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+// Render gallery items.
+gallery.insertAdjacentHTML(
+    "beforeend",
+    galleryItems
+      .map(
+        ({ preview, original, description }) =>
+          `
+          <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+          `
+      )
+      .join("")
+  );
+  
+  // Simple-lightbox init.
+  const viem = { captionsData: "alt", captionDelay: 450 };
+  
+  var lightbox = new SimpleLightbox(".gallery a", viem);
